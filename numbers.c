415565f3b7c2678339bb8a1fd89dede0797bdf32
ikhumwa edmond <edmondmulera55@gmail.com>
2023-05-24 08:26:01 +0300

    simple shell

--- numbers.c
+++ numbers.c
@@ -1,19 +1,12 @@
 #include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	_putchar('H');
-	_putchar('e');
-	_putchar('l');
-	_putchar('l');
-	_putchar('o');
-	_putchar(',');
-	_putchar(' ');
-	_putchar('w');
-	_putchar('o');
-	_putchar('r');
-	_putchar('l');
-	_putchar('d');
-	_putchar('!');
-	_putchar('\n');
-	return 0;
+	printf("Hello, world!\n");
+	return (0);
 }
