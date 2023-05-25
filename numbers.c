65efda9135a61a60e2970dfb835afed7ceef1438
ikhumwa edmond <edmondmulera55@gmail.com>
2023-05-25 09:59:02 +0300

    update

--- numbers.c
+++ numbers.c
@@ -1,8 +1,27 @@
 #include "main.h"
 
-void print_numbers(int number) {
-    int i = 0;
-    i += _putchar('-');
-    i += _putchar(number % 10 + '0');
-    printf("i: %d\n", i); 
+/**
+ * print_numbers - Prints numbers, used to print the error message
+ * @n: number to print
+ * Return: Number of characters printed
+ */
+
+int print_numbers(int n)
+{
+	unsigned int number;
+	int i = 0;
+
+	if (n < 0)
+	{
+		number = -n;
+		i = i + _putchar('-');
+	}
+	else
+	{
+		number = n;
+	}
+	if (number / 10 != 0)
+		i = i + print_numbers(number / 10);
+	i = i + _putchar(number % 10 + '0');
+	return (i);
 }
